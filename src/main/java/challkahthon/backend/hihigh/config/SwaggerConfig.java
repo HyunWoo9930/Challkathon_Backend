d502ee38c22fe5b593bcaad104e6e997c920280f
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-06-22 19:01:39 +0900

    Feat : swagger http/https 수정

--- src/main/java/challkahthon/backend/hihigh/config/SwaggerConfig.java
+++ src/main/java/challkahthon/backend/hihigh/config/SwaggerConfig.java
@@ -2,12 +2,18 @@ package challkahthon.backend.hihigh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,8 +28,27 @@ public class SwaggerConfig {
 				.name("HyunWoo9930")
 				.email("hw62459930@gmail.com"));
 
+		// Create servers for both HTTP and HTTPS
+		Server httpServer = new Server()
+			.url("http://{host}")
+			.description("HTTP Server");
+
+		Server httpsServer = new Server()
+			.url("https://{host}")
+			.description("HTTPS Server");
+
+		io.swagger.v3.oas.models.servers.ServerVariables httpVariables = new io.swagger.v3.oas.models.servers.ServerVariables();
+		io.swagger.v3.oas.models.servers.ServerVariable hostVariable = new io.swagger.v3.oas.models.servers.ServerVariable();
+		hostVariable.setDefault("localhost:8080");
+		hostVariable.setDescription("Server host (and port)");
+		httpVariables.addServerVariable("host", hostVariable);
+
+		httpServer.setVariables(httpVariables);
+		httpsServer.setVariables(httpVariables);
+
 		return new OpenAPI()
 			.info(info)
+			.servers(java.util.List.of(httpServer, httpsServer))
 			.addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
 			.components(new io.swagger.v3.oas.models.Components()
 				.addSecuritySchemes("Bearer Authentication", new SecurityScheme()
