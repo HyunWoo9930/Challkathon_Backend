a55eb930a602f200004292782065521c35cd14ce
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-06-22 19:22:01 +0900

    Fix : redirectUri 수정

--- src/main/java/challkahthon/backend/hihigh/jwt/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/challkahthon/backend/hihigh/jwt/OAuth2AuthenticationSuccessHandler.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtTokenProvider tokenProvider;
-    private final String redirectUri = "http://localhost:5000";
+    private final String redirectUri = "/";
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
